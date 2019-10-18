f38331ab4ccee82a5ed2f34b216bf67b43ee5ec7
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-17 19:29:38 -0700

    feat: Implement typed read-only variable

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
+    val playerName: String = "Estragon"
     var experiencePoints: Int = 5
+    experiencePoints += 5
     println(experiencePoints)
+    println(playerName)
 }
