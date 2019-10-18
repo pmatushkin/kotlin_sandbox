2370327203f431e99039724cfba7e2c6bd3f1f06
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-17 19:52:00 -0700

    feat: Implement Chapter 2 challenges

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -3,7 +3,15 @@ const val MAX_EXPERIENCE: Int = 5000
 fun main(args: Array<String>) {
     val playerName = "Estragon"
     var experiencePoints = 5
+    var hasSteed = false
+    var gold = 50
+    var pubName = "The Unicorn's Horn"
+    var pubDrinks = setOf( "mead", "wine", "LaCroix" )
+
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
+    println(hasSteed)
+    println(pubDrinks)
+    println(playerName.reversed())
 }
