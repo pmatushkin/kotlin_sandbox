06705d1a745c11ae3ae243cfa660ef104116a12e
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-17 19:34:43 -0700

    feat: Remove explicit type declarations

--- src/TypeIntro.kt
+++ src/TypeIntro.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    val playerName: String = "Estragon"
-    var experiencePoints: Int = 5
+    val playerName = "Estragon"
+    var experiencePoints = 5
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
