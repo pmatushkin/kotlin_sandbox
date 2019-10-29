6bfc28147deb7f99732b758f223f15767c80c49e
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-28 19:36:51 -0700

    feat: Use type inference when defining parameters of anonymous function

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
