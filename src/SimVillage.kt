e361261a55d937f5143d10d41b0b808f6fd41f17
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-28 19:44:51 -0700

    feat: Apply shorthand syntax when passing anonymous function as a parameter

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,11 +1,9 @@
 fun main(args: Array<String>) {
-    val greetingFunction = { playerName: String, numBuildings: Int ->
+    runSimulation("Guyal") { playerName, numBuildings ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-
-    runSimulation("Guyal", greetingFunction)
 }
 
 fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
